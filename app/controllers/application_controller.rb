eaae9de04be9764b948a4206b008827a14a13ecb
Thanawin <thanawinrst@gmail.com>
2021-06-25 18:27:02 +0700

    [Task-4] Fix code format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   protected
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
       user_params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
