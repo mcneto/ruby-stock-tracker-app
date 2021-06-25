aab34253d94b9450328525195e38adb835fc3774
Thanawin <thanawinrst@gmail.com>
2021-06-25 20:57:44 +0700

    [Task-5] Update User model file

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :first_name, :last_name, presence: true, length: { maximum:25 }
+  validates :first_name, :last_name, presence: true, length: { maximum: 25 }
 end
