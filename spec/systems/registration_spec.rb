3ee5f0c9eca325c73c3f365ee49ce10f14b3a033
Thanawin Rattanamanee <70894675+mcneto@users.noreply.github.com>
2021-07-19 22:48:15 +0700

    [Task-4] Update test name

--- spec/systems/registration_spec.rb
+++ spec/systems/registration_spec.rb
@@ -9,14 +9,14 @@ describe 'Registration', type: :system do
       expect(page).to have_content 'Welcome! You have signed up successfully.'
     end
 
-    it 'create new user' do
+    it 'create a new user' do
       sign_up_user 'test@example.com', 'test1234', 'test1234'
       expect(User.count).to eq(1)
     end
   end
 
   context 'given no inputs' do
-    it 'does not create new user' do
+    it 'does not create a new user' do
       visit new_user_registration_path
 
       click_button 'Sign up'
@@ -25,21 +25,21 @@ describe 'Registration', type: :system do
   end
 
   context 'given an invalid email input' do
-    it 'does not create new user' do
+    it 'does not create a new user' do
       sign_up_user 'test.com', 'test1234', 'test1234'
       expect(User.count).to eq(0)   # equal 1 when new user was created
     end
   end
 
   context 'given an invalid password input' do
-    it 'displays error message' do
+    it 'displays an error message' do
       sign_up_user 'test@example.com', 'test1', 'test1'
       expect(find('form')).to have_selector('div#error_explanation')
     end
   end
 
   context 'given a wrong password confirmation ' do
-    it 'displays error message' do
+    it 'displays an error message' do
       sign_up_user 'test@example.com', 'test1234', 'test1246'
       expect(find('form')).to have_selector('div#error_explanation')
     end
