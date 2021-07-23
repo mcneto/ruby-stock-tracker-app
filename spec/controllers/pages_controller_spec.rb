0837ca7042a06e85cc2b4e8e86b41a1b1f79a0f5
Thanawin Rattanamanee <70894675+mcneto@users.noreply.github.com>
2021-07-23 17:14:46 +0700

    [Task-6] Fix sign in test name

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,14 +4,14 @@ require 'rails_helper'
 
 describe PagesController, type: :controller do
   describe 'GET #index' do
-    context 'when unauthorized user access to main screen' do
-      it 'returns a redirect response' do
+    context 'given unauthorized user tries to access the main page' do
+      it 'redirects to the sign-in page' do
         get :index
         expect(response).to have_http_status(:redirect)
       end
     end
 
-    context 'when signed in user access to main screen' do
+    context 'given authorized user tries to access the main page' do
       it 'returns a success response' do
         sign_in_user
         get :index
--- spec/systems/sign_in_spec.rb
+++ spec/systems/sign_in_spec.rb
@@ -3,20 +3,20 @@
 require 'rails_helper'
 
 describe 'Sign in', type: :system do
-  context 'given the valid inputs' do
+  context 'given valid credentials' do
     it 'displays successful sign in message' do
       sign_in_ui
       expect(page).to have_content 'Signed in successfully.'
     end
 
-    it 'redirect to main screen' do
+    it 'redirect to the main screen' do
       sign_in_ui
       expect(page).to have_current_path(root_path)
     end
   end
 
   context 'given no inputs' do
-    it 'does not redirect to main screen' do
+    it 'does not redirect to the main screen' do
       visit root_path
 
       click_button 'Log in'
@@ -25,14 +25,14 @@ describe 'Sign in', type: :system do
   end
 
   context 'given an invalid email input' do
-    it 'does not redirect to main screen' do
+    it 'does not redirect to the main screen' do
       sign_in_ui 'test@example', ''
       expect(page).to have_current_path(new_user_session_path)
     end
   end
 
   context 'given an invalid password input' do
-    it 'does not redirect to main screen' do
+    it 'does not redirect to the main screen' do
       sign_in_ui '', 'test12'
       expect(page).to have_current_path(new_user_session_path)
     end
