b242fc3bcef27b89236540d97b10db058fc6e24c
Thanawin Rattanamanee <70894675+mcneto@users.noreply.github.com>
2021-07-23 17:15:36 +0700

    [Task-8] Fix sign out test name

--- spec/systems/sign_out_spec.rb
+++ spec/systems/sign_out_spec.rb
@@ -3,8 +3,8 @@
 require 'rails_helper'
 
 describe 'Sign out', type: :system do
-  context 'given the sign out option is clicked' do
-    it 'redirect to sign in screen' do
+  context 'given the sign-out link is clicked' do
+    it 'redirect to the sign-in page' do
       login
       visit root_path
       click_link 'Sign Out'
