d2c670646ac150f1c4754781dfcefadc9f3c6003
Thanawin Rattanamanee <70894675+mcneto@users.noreply.github.com>
2021-08-10 12:28:41 +0700

    [Task-10] Configure permitted parameters for Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
       user_params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
+
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+    end
   end
 end
